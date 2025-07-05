e183b1ac90a445752609bb121ba84b17717772b9
Christian Heinrich <coffeepyros@gmail.com>
2025-07-05 12:46:24 +0200

    added control panel for number of circles and speed

--- sketch.js
+++ sketch.js
@@ -1,7 +1,31 @@
-// Configuration in Global Scope
+// Global Configuration
 const scale = 300; // controls size of path circle
 const r = scale * 2;
-const step = 30; // controls amount of circles
+
+// ---------- CONTROL PANEL ----------
+
+// Number of "Planets" (Circles)
+const numPlanets = document.getElementById("numPlanets");
+const numPlanetsValue = document.getElementById("numPlanetsValue");
+let planets = numPlanets.value; // initial value
+let step = 360 / planets; // initial value
+
+numPlanets.addEventListener("input", (e) => {
+    numPlanetsValue.innerText = e.target.value;
+    planets = e.target.value;
+    step = 360 / planets;
+});
+
+// Speed of Planets
+const speedElement = document.getElementById("speed");
+let speed = speedElement.value / 100;
+
+speedElement.addEventListener("input", (e) => {
+    speedValue.innerText = e.target.value + "%";
+    speed = speedElement.value / 100;
+});
+
+// ---------- P5 ----------
 
 function setup() {
     createCanvas(1080, 1080); // full-hd square
@@ -16,10 +40,9 @@ function draw() {
     noFill();
     circle(540, 540, r);
 
-    // console.log(frameCount);
-    for (let angle = 0; angle <= 360; angle += step) {
-        x = sin(angle + frameCount / 4);
-        y = cos(angle + frameCount / 4);
+    for (let angle = 0; angle < 360; angle += step) {
+        x = sin(angle + frameCount * speed);
+        y = cos(angle + frameCount * speed);
         fill(167, 139, 250); // TODO: random color shift?
         circle(540 + x * scale, 540 - y * scale, 67);
     }
